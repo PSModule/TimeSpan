a9c9089b8a61b7152742619a7712c6b807a0811f
Marius Storhaug <marstor@hotmail.com>
2025-02-19 20:11:32 +0100

    Refactor Format-TimeSpan function to improve parameter handling and simplify unit mapping; update UnitMap with tick values for time units

--- src/variables/private/UnitMap.ps1
+++ src/variables/private/UnitMap.ps1
@@ -1,67 +1,74 @@
-﻿$script:UnitMap = @{
+﻿$script:UnitMap = [ordered]@{
     'Millennia'    = @{
         Singular     = 'millennium'
         Plural       = 'millennia'
         Abbreviation = 'mil'
+        Ticks        = 315569520000000000
     }
     'Centuries'    = @{
         Singular     = 'century'
         Plural       = 'centuries'
         Abbreviation = 'cent'
+        Ticks        = 31556952000000000
     }
     'Decades'      = @{
         Singular     = 'decade'
         Plural       = 'decades'
         Abbreviation = 'dec'
+        Ticks        = 3155695200000000
     }
     'Years'        = @{
         Singular     = 'year'
         Plural       = 'years'
         Abbreviation = 'y'
+        Ticks        = 315569520000000
     }
     'Months'       = @{
         Singular     = 'month'
         Plural       = 'months'
         Abbreviation = 'mo'
+        Ticks        = 26297460000000
     }
     'Weeks'        = @{
         Singular     = 'week'
         Plural       = 'weeks'
         Abbreviation = 'w'
+        Ticks        = 6048000000000
     }
     'Days'         = @{
         Singular     = 'day'
         Plural       = 'days'
         Abbreviation = 'd'
+        Ticks        = 864000000000
     }
     'Hours'        = @{
         Singular     = 'hour'
         Plural       = 'hours'
         Abbreviation = 'h'
+        Ticks        = 36000000000
     }
     'Minutes'      = @{
         Singular     = 'minute'
         Plural       = 'minutes'
         Abbreviation = 'm'
+        Ticks        = 600000000
     }
     'Seconds'      = @{
         Singular     = 'second'
         Plural       = 'seconds'
         Abbreviation = 's'
+        Ticks        = 10000000
     }
     'Milliseconds' = @{
         Singular     = 'millisecond'
         Plural       = 'milliseconds'
         Abbreviation = 'ms'
+        Ticks        = 10000
     }
     'Microseconds' = @{
         Singular     = 'microsecond'
         Plural       = 'microseconds'
         Abbreviation = 'us'
-    }
-    'Nanoseconds'  = @{
-        Singular     = 'nanosecond'
-        Plural       = 'nanoseconds'
-        Abbreviation = 'ns'
+        Ticks        = 10
     }
 }
